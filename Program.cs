2adf88e0fac52c94006556f43bbf74bff66f7d5f
Kyle Cox <cox.kyle@gmail.com>
2021-08-22 13:58:58 -0400

    First commit for deliverable one

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your age? ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("What is the current year? ");
+            int year = int.Parse(Console.ReadLine());
+
+            int birthYear = year - age;
+
+            Console.WriteLine(name + ", if you're " + age + " years old, you were probably born in " + birthYear + ".");
+        }
+    }
+}
